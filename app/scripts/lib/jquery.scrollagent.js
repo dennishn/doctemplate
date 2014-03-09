187ce4f7e7e9ef0d20032cfbb3e08a32baf3f7c9
Dennis Haulund Nielsen <dhni@nodes.dk>
2014-03-10 00:01:17 +0100

    UI
    Small and large devices.

--- app/scripts/lib/jquery.scrollagent.js
+++ app/scripts/lib/jquery.scrollagent.js
@@ -19,70 +19,70 @@
 (function($) {
 
   $.fn.scrollagent = function(options, callback) {
-    // Account for $.scrollspy(function)
-    if (typeof callback === 'undefined') {
-      callback = options;
-      options = {};
-    }
+	// Account for $.scrollspy(function)
+	if (typeof callback === 'undefined') {
+	  callback = options;
+	  options = {};
+	}
 
-    var $sections = $(this);
-    var $parent = options.parent || $(window);
+	var $sections = $(this);
+	var $parent = options.parent || $(window);
 
-    // Find the top offsets of each section
-    var offsets = [];
-    $sections.each(function(i) {
-      var offset = $(this).attr('data-anchor-offset') ?
-        parseInt($(this).attr('data-anchor-offset'), 10) :
-        (options.offset || 0);
+	// Find the top offsets of each section
+	var offsets = [];
+	$sections.each(function(i) {
+	  var offset = $(this).attr('data-anchor-offset') ?
+		parseInt($(this).attr('data-anchor-offset'), 10) :
+		(options.offset || 0);
 
-      offsets.push({
-        top: $(this).offset().top + offset,
-        id: $(this).attr('id'),
-        index: i,
-        el: this
-      });
-    });
+	  offsets.push({
+		top: ($(this).offset().top + offset) + 120,
+		id: $(this).attr('id'),
+		index: i,
+		el: this
+	  });
+	});
 
-    // State
-    var current = null;
-    var height = null;
-    var range = null;
+	// State
+	var current = null;
+	var height = null;
+	var range = null;
 
-    // Save the height. Do this only whenever the window is resized so we don't
-    // recalculate often.
-    $(window).on('resize', function() {
-      height = $parent.height();
-      range = $(document).height();
-    });
+	// Save the height. Do this only whenever the window is resized so we don't
+	// recalculate often.
+	$(window).on('resize', function() {
+	  height = $parent.height();
+	  range = $(document).height();
+	});
 
-    // Find the current active section every scroll tick.
-    $parent.on('scroll', function() {
-      var y = $parent.scrollTop();
-      y += height * (0.3 + 0.7 * Math.pow(y/range, 2));
+	// Find the current active section every scroll tick.
+	$parent.on('scroll', function() {
+	  var y = $parent.scrollTop();
+	  y += height * (0.3 + 0.7 * Math.pow(y/range, 2));
 
-      var latest = null;
+	  var latest = null;
 
-      for (var i in offsets) {
-        if (offsets.hasOwnProperty(i)) {
-          var offset = offsets[i];
-          if (offset.top < y) latest = offset;
-        }
-      }
+	  for (var i in offsets) {
+		if (offsets.hasOwnProperty(i)) {
+		  var offset = offsets[i];
+		  if (offset.top < y) latest = offset;
+		}
+	  }
 
-      if (latest && (!current || (latest.index !== current.index))) {
-        callback.call($sections,
-          latest ? latest.id : null,
-          current ? current.id : null,
-          latest ? latest.el : null,
-          current ? current.el : null);
-        current = latest;
-      }
-    });
+	  if (latest && (!current || (latest.index !== current.index))) {
+		callback.call($sections,
+		  latest ? latest.id : null,
+		  current ? current.id : null,
+		  latest ? latest.el : null,
+		  current ? current.el : null);
+		current = latest;
+	  }
+	});
 
-    $(window).trigger('resize');
-    $parent.trigger('scroll');
+	$(window).trigger('resize');
+	$parent.trigger('scroll');
 
-    return this;
+	return this;
   };
 
 })(jQuery);
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,90 @@
-//Add active class to flatdoc navigation as we scroll along!
+var toggleSidebar = function() {
+
+	$('.menubar').toggleClass('is-open');
+
+	$('.toggle-topbar').toggle();
+
+	if( $('body').hasClass('navigation-is-open') ) {
+
+		$('body').removeClass('navigation-is-open');
+
+	} else {
+
+		$('body').addClass('navigation-is-open');
+
+		$('body.navigation-is-open').one('click', function(event) {
+			$('.menubar').removeClass('is-open');
+			$('body').removeClass('navigation-is-open');
+			$('.toggle-topbar').show();
+		});
+
+		$('.menubar a').one('click', function(event) {
+			$('.menubar').removeClass('is-open');
+			$('body').removeClass('navigation-is-open');
+			$('.toggle-topbar').show();
+		});
+
+	}
+
+};
+
+var toggleTopbar = function() {
+
+	var currentOffset = $('body').scrollTop();
+
+	$('.primary-navigation').toggleClass('is-open');
+
+	$('.toggle-sidebar').toggle();
+
+	$('.menubar').removeClass('is-open');
+
+	if( $('body').hasClass('navigation-is-open') ) {
+		console.log('ttif');
+		$('body').removeClass('navigation-is-open');
+
+	} else {
+		console.log('ttelse');
+		$('body').addClass('navigation-is-open');
+
+		$('.toggle-topbar').one('click', function(event) {
+
+			$('body').scrollTop(currentOffset);
+			$('body').removeClass('navigation-is-open');
+		});
+
+		$('.top-bar-section a').one('click', function(event) {
+
+			$('.primary-navigation').removeClass('is-open');
+			$('body').removeClass('navigation-is-open');
+			$('body').scrollTop(currentOffset);
+			$('.toggle-sidebar').show();
+		});
+
+	}
+
+}
+
+var scrollToElement = function(el) {
+	var _this = el;
+
+	var targetId = _this.attr('href');
+
+	targetOffset = ($(targetId).offset().top - 60);
+
+	window.scrollTo(0, targetOffset)
+
+}
+
+var setActiveMenu = function() {
+	var path = window.location.pathname;
+	var targetString = path.substring(1);
+	// console.log(targetString);
+	$(document).find('a[href="' + targetString + '"]').parent().addClass('active');
+}
+
 $(document).on('flatdoc:ready', function() {
-	$("h1, h2, h3").scrollagent(function(cid, pid, currentElement, previousElement) {
+	//Add active class to flatdoc navigation as we scroll along!
+	$("h1, h2, h3, h4, h5, h6").scrollagent(function(cid, pid, currentElement, previousElement) {
 		if (pid) {
 			$("[href='#"+pid+"']").removeClass('active');
 		}
@@ -9,21 +93,27 @@ $(document).on('flatdoc:ready', function() {
 		}
 	});
 
-	$(function() {
-		var $sidebar = $('.menubar'),
-			$content = $('.content');
-		var elTop;
-
-		$(window)
-		.on('resize.sidestick', function() {
-			elTop = $sidebar.offset().top;
-			$(window).trigger('scroll.sidestick');
-		})
-		.on('scroll.sidestick', function() {
-			var scrollY = $(window).scrollTop();
-			$sidebar.toggleClass('fixed', (scrollY >= elTop));
-			$content.toggleClass('menu-is-fixed', (scrollY >= elTop));
-		})
-		.trigger('resize.sidestick');
+	$('.menubar a').on('click', function(event) {
+		event.preventDefault();
+		scrollToElement($(this));
 	});
+
+
+});
+
+$(document).ready(function(){
+	$(document).foundation();
+
+	setActiveMenu();
+
+	$('.toggle-sidebar').on('click', function(event) {
+		event.preventDefault();
+		event.stopPropagation();
+		toggleSidebar();
+	});
+	$('.toggle-topbar').on('click', function(event) {
+		event.preventDefault();
+		toggleTopbar();
+	});
+
 });
